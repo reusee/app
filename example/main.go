03bf74177867168f3c17627c5906a10ac7929d80
reus <reusee@gmail.com>
2015-03-13 20:56:38 +0800

    do not use relative import path

--- example/main.go
+++ example/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	app ".."
+	"github.com/reusee/app"
 )
 
 var application = app.New()
--- example/mod_foo.go
+++ example/mod_foo.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	app ".."
+	"github.com/reusee/app"
 )
 
 func init() {
